b982c353609091e62d369130125cb6a43a010caf
Joshua Villasis (CSUF) <42751215+JJVillasis@users.noreply.github.com>
2022-12-01 15:50:54 -0800

    Fix: Saves local theme choice

---
+++ setTheme.js
@@ -0,0 +1,27 @@
+var prevState = parseInt(localStorage.getItem("clickState"));
+console.log(typeof localStorage.getItem("clickState"));
+
+switch(prevState) {
+    case 1:
+        document.body.style.backgroundColor = "black";
+        break;
+    case 2:
+        document.body.style.backgroundColor = "brown";
+        break;
+    case 3:
+        document.body.style.backgroundColor = "green";
+        break;
+    case 4:
+        document.body.style.backgroundColor = "rgb(100, 50, 119)";
+        break;
+    case 5:
+        document.body.style.backgroundColor = "rgb(200, 68, 112)";
+        break;
+    case 6:
+        document.body.style.backgroundColor = "rgb(80, 20, 122)";
+        break;
+    default:
+        console.log("Here")
+        document.body.style.backgroundColor = "rgb(14, 58, 116)";
+        break;
+}
--- themes.js
+++ themes.js
@@ -14,75 +14,39 @@
 |  https://github.com/Arbalest007/CSUF-Events-Extension                                               |
 ------------------------------------------------------------------------------------------------------*/
 
-const btn = document.getElementById("btn");
-let clickState = 0;
+const themeBtn = document.getElementById("btn");
+var clickState = localStorage.getItem("clickState");
+console.log("Saved Click State = " + clickState);
 
-btn.addEventListener("click", function onClick(event) {
+themeBtn.addEventListener("click", function onClick(event) {
   clickState++;
+  clickState %= 7;
 
-  if (clickState == 1) {
-    var foobarElement = document.getElementById("foobar");
-    foobarElement.style.backgroundImage = "";
-    foobarElement.style.background = "";
-    foobarElement.style.backgroundUrl = "";
-    foobarElement.style.backgroundColor = "";
+  console.log("Click State = " + clickState)
 
-    document.body.style.backgroundColor = "black";
-    localStorage.setItem("clickState", clickState);
-  } else if (clickState == 2) {
-    var foobarElement = document.getElementById("foobar");
-    foobarElement.style.backgroundImage = "";
-    foobarElement.style.background = "";
-    foobarElement.style.backgroundUrl = "";
-    foobarElement.style.backgroundColor = "";
-
-    document.body.style.backgroundColor = "brown";
-    localStorage.setItem("event", "cyan");
-  } else if (clickState == 3) {
-    var foobarElement = document.getElementById("foobar");
-    foobarElement.style.backgroundImage = "";
-    foobarElement.style.background = "";
-    foobarElement.style.backgroundUrl = "";
-    foobarElement.style.backgroundColor = "";
-
-    document.body.style.backgroundColor = "green";
-    localStorage.setItem("clickState", clickState);
-  } else if (clickState == 4) {
-    var foobarElement = document.getElementById("foobar");
-    foobarElement.style.backgroundImage = "";
-    foobarElement.style.background = "";
-    foobarElement.style.backgroundUrl = "";
-    foobarElement.style.backgroundColor = "";
-
-    document.body.style.backgroundColor = "rgb(100, 50, 119)";
-    localStorage.setItem("clickState", clickState);
-  } else if (clickState == 5) {
-    var foobarElement = document.getElementById("foobar");
-    foobarElement.style.backgroundImage = "";
-    foobarElement.style.background = "";
-    foobarElement.style.backgroundUrl = "";
-    foobarElement.style.backgroundColor = "";
-
-    document.body.style.backgroundColor = "rgb(200, 68, 112)";
-    localStorage.setItem("clickState", clickState);
-  } else if (clickState == 6) {
-    var foobarElement = document.getElementById("foobar");
-    foobarElement.style.backgroundImage = "";
-    foobarElement.style.background = "";
-    foobarElement.style.backgroundUrl = "";
-    foobarElement.style.backgroundColor = "";
-
-    document.body.style.backgroundColor = "rgb(80, 20, 122)";
-    localStorage.setItem("clickState", clickState);
-  } else if (clickState == 7) {
-    var foobarElement = document.getElementById("foobar");
-    foobarElement.style.backgroundImage = "";
-    foobarElement.style.background = "";
-    foobarElement.style.backgroundUrl = "";
-    foobarElement.style.backgroundColor = "";
-
-    document.body.style.backgroundColor = "rgb(14, 58, 116)";
-    localStorage.setItem("clickState", clickState);
-    clickState = 0;
+  switch(clickState) {
+    case 1:
+      document.body.style.backgroundColor = "black";
+      break;
+    case 2:
+      document.body.style.backgroundColor = "brown";
+      break;
+    case 3:
+      document.body.style.backgroundColor = "green";
+      break;
+    case 4:
+      document.body.style.backgroundColor = "rgb(100, 50, 119)";
+      break;
+    case 5:
+      document.body.style.backgroundColor = "rgb(200, 68, 112)";
+      break;
+    case 6:
+      document.body.style.backgroundColor = "rgb(80, 20, 122)";
+      break;
+    default:
+      document.body.style.backgroundColor = "rgb(14, 58, 116)";
+      break;
   }
+
+  localStorage.setItem("clickState", clickState);
 });
